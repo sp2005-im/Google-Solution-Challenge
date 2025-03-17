0843ac533a11f33181ed61666be5c9011b7b4fb2
Srihari Prasad <sp2005im@gmail.com>
2025-03-17 19:19:15 +0530

    Update and rename FAISS_RAG_testing.py to RAG_Testing.py

---
+++ FIN-RAG/Document-Retrieval/RAG_Testing.py
@@ -0,0 +1,29 @@
+#Sri Rama Jayam
+#Running the FAISS_RAG_1.py
+from FAISS_RAG_1 import FinancialAssistant
+import os
+from dotenv import load_dotenv 
+
+#Load the environment variables
+load_dotenv()
+
+openai_api_key = os.getenv("OPENAI_API_KEY") #Ensure the .env file is in the directory in which you are running the code
+data_path = os.getenv("DATA_PATH")  # Load data path from environment variable
+faiss_path = os.getenv("FAISS_PATH")  # Load FAISS path from environment variable
+
+# Check if paths are set
+if not data_path or not faiss_path:
+    raise ValueError("DATA_PATH and FAISS_PATH must be set in environment variables.")
+
+# Initialize the Financial Assistant
+assistant = FinancialAssistant(data_path, faiss_path)
+
+# Sample Query - Can be replaced with any query
+query = "What is the difference between savings and investment"
+response = assistant.generate_response(query)
+
+# Print results
+print("\nQuery: ", query)
+print("\nResponse: ", response)
+
+
